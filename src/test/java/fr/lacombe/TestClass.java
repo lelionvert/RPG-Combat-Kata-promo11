ea6de2767f4cdb2e2ad3c25f6146f9f73b7ba3a7
20Bak <back.to.bak@gmail.com>
2019-11-08 11:44:40 +0100

    SetUp environment

---
+++ src/test/java/fr/lacombe/TestClass.java
@@ -0,0 +1,13 @@
+package fr.lacombe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TestClass {
+
+    @Test
+    void setUpEnvironment(){
+        assertThat(true).isTrue();
+    }
+}
